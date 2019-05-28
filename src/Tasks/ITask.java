da14bc91ff710261506c17f0b0368bd5f5c86883
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-05-28 21:17:58 +1000

    add init

--- src/Tasks/ITask.java
+++ src/Tasks/ITask.java
@@ -1,9 +1,12 @@
 package Tasks;
 
+import Exceptions.CannotStartException;
 import Exceptions.TaskFinishedException;
 
 interface ITask {
 
     void runTask() throws TaskFinishedException;
+
+    void init() throws CannotStartException;
 
 }
