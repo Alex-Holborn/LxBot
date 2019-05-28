eb58c2953542f039ff69f39be171ede9a29f69a7
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-05-28 21:18:34 +1000

    update onLoop

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import Exceptions.CannotStartException;
+import Exceptions.OutOfTasksException;
 import Exceptions.TaskFinishedException;
 import Tasks.LxTask;
 import org.osbot.rs07.script.Script;
@@ -14,6 +16,8 @@ public class Main extends Script {
 
     private LxTask[] taskQueue;
 
+    private LxTask activeTask;
+
     public void onStart(){
         new Gui(this);
     }
@@ -21,28 +25,61 @@ public class Main extends Script {
     @Override
     public int onLoop(){
         if(hasStarted){
-            if(taskQueue != null && taskQueue[0] != null){
-                try{
-                    taskQueue[0].runTask();
-                }catch(TaskFinishedException e){
-                    finishCurrentTask(e);
+            if(taskQueue != null ){
+                if(activeTask != null){
+                    if(activeTask.getHasRanInit()) {
+                        try {
+                            activeTask.runTask();
+                        } catch (TaskFinishedException e) {
+                            try {
+                                finishCurrentTask(e);
+                                updateActiveTask();
+                            } catch (OutOfTasksException o) {
+                                finishBot(o);
+                            }
+                        }
+                    } else{
+                        try {
+                            activeTask.init();
+                            activeTask.setHasRanInit();
+                        } catch(CannotStartException e){
+                            try{
+                                finishCurrentTask(new TaskFinishedException(this, activeTask, "Changing Tasks..."));
+                            } catch(OutOfTasksException o){
+                                finishBot(o);
+                            }
+                        }
+                    }
                 }
             }
         }
         return 100;
     }
 
-    public void createTaskQueue(ArrayList<LxTask> tasks){
+    void updateActiveTask(){
+        activeTask = taskQueue[0];
+    }
+
+    void createTaskQueue(ArrayList<LxTask> tasks){
         taskQueue = new LxTask[tasks.size()];
         for(int i = 0; i < tasks.size(); i++){
             taskQueue[i] = tasks.get(i);
         }
     }
 
-    private void finishCurrentTask(TaskFinishedException e){
-        LxTask[] newQ = new LxTask[taskQueue.length - 1];
-        System.arraycopy(taskQueue, 1, newQ,0, newQ.length);
-        log(e.getThrownTask().getTaskName() + " has finished!");
-        taskQueue = newQ;
+    private void finishBot(OutOfTasksException o){
+        log(o.getMessage());
+        stop(true);
+    }
+
+    private void finishCurrentTask(TaskFinishedException e) throws OutOfTasksException{
+        if(taskQueue.length <= 1){
+            throw new OutOfTasksException(this, activeTask, "Out of Available Tasks...");
+        }else {
+            LxTask[] newQ = new LxTask[taskQueue.length - 1];
+            System.arraycopy(taskQueue, 1, newQ, 0, newQ.length);
+            log(e.getThrownTask().getTaskName() + " has finished!");
+            taskQueue = newQ;
+        }
     }
 }
